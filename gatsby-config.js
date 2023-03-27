2f89cbf6e87c7d18699018bff57148fa505d75bf
nalbaeksoo <bae@junan.kr>
2023-03-27 21:04:43 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/src/site/notes`,
-        rootNote: `/start_index.md`,
+        rootNote: `/start_index`,
       },
     },
     `gatsby-plugin-netlify`
