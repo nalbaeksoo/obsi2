93538cbfca93093bd70ee2a78187283512f2849e
nalbaeksoo <bae@junan.kr>
2023-03-27 18:55:44 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/src/site/notes`,
-        rootNote: `https://github.com/nalbaeksoo/obsi2/tree/master/src/site/notes/start_index.md`,
+        rootNote: `./src/site/notes/start_index.md`,
       },
     },
     `gatsby-plugin-netlify`
